eedc26ec2fefb98d81f33539f32336ebe7d0a086
aaronmicahgreen <73668177+aaronmicahgreen@users.noreply.github.com>
2021-05-06 11:46:41 -0400

    update documentation in convertPuzzleToClues.m

--- convertPuzzleToClues.m
+++ convertPuzzleToClues.m
@@ -4,11 +4,10 @@ function clues = convertPuzzleToClues(puzzle)
 %                      (row number, column number, entry). 0's in the
 %                      puzzle will not be converted to clues.
 %%*************************************************************************
-    if isequal(size(puzzle),[9,9]) % 9-by-9 clues
-        % Convert to 81-by-3
-        [SM,SN] = meshgrid(1:9); % make i,j entries
-        puzzle = [SN(:),SM(:),puzzle(:)]; % i,j,k rows
-        % Now delete zero rows
+    if isequal(size(puzzle),[9,9])
+        [SM,SN] = meshgrid(1:9);
+        puzzle = [SN(:),SM(:),puzzle(:)];
+        % Delete rows whose entry is 0
         [rrem,~] = find(puzzle(:,3) == 0);
         puzzle(rrem,:) = [];
     end
